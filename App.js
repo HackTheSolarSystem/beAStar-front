e6b32db5bc51d1c84de2837b0417e41210cd5250
Cameron Yick <cameron.yick@enigma.com>
2019-02-10 10:28:10 -0500

    [ui] add menu to select which star color to be

--- App.js
+++ App.js
@@ -1,43 +1,167 @@
-/**
- * Copyright (c) 2015-present, Viro Media, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- *
- */
-'use strict';
+import React, { Component } from "react";
+import { ViroARSceneNavigator } from "react-viro";
 
-import React, { Component } from 'react';
+import { StyleSheet, Text, TouchableHighlight, View } from "react-native";
+import { VIRO_API_KEY } from "./credentials";
 
-import {
-  AppRegistry,
-} from 'react-native';
+var sharedProps = {
+  apiKey: VIRO_API_KEY
+};
 
-import {
-  ViroARSceneNavigator,
-} from 'react-viro';
+// Sets the default scene you want for AR and VR
+var InitialARScene = require("./js/ARCarDemo");
 
-var createReactClass = require('create-react-class');
+var UNSET = "UNSET";
+var AR_NAVIGATOR_TYPE = "AR";
 
-var apiKey = "608C47E8-0E44-41D2-BB4B-79DFF316C0E4";
+// This determines which type of experience to launch in, or UNSET, if the user should
+// be presented with a choice of AR or VR. By default, we offer the user a choice.
+var defaultNavigatorType = UNSET;
 
-var arScenes = {
-  'player1': require('./js/ARCarDemo'),
-  'player2': require('./js/ARCarDemo'),
-}
+const COLORS = {
+  // enum of marker identifiers
+  A: "A",
+  B: "B"
+};
 
-var ViroPlayerSceneNavigator = createReactClass({
-  render: function() {
+export default class ViroPlayerSceneNavigator extends Component {
+  constructor() {
+    super();
 
+    this.state = {
+      navigatorType: defaultNavigatorType,
+      color: COLORS.A, // default color
+      sharedProps: sharedProps
+    };
+    this._getExperienceSelector = this._getExperienceSelector.bind(this);
+    this._getARNavigator = this._getARNavigator.bind(this);
+    this._getExperienceButtonOnPress = this._getExperienceButtonOnPress.bind(
+      this
+    );
+    this._exitViro = this._exitViro.bind(this);
+  }
+  // Replace this function with the contents of _getVRNavigator() or _getARNavigator()
+  // if you are building a specific type of experience.
+  render() {
+    if (this.state.navigatorType == UNSET) {
+      return this._getExperienceSelector();
+    } else if (this.state.navigatorType == AR_NAVIGATOR_TYPE) {
+      return this._getARNavigator();
+    }
+  }
+
+  // Presents the user with a choice of an AR or VR experience
+  _getExperienceSelector() {
+    return (
+      <View style={localStyles.outer}>
+        <View style={localStyles.inner}>
+          <Text style={localStyles.titleText}>Pick your star color!</Text>
+
+          <TouchableHighlight
+            style={localStyles.buttons}
+            onPress={this._getExperienceButtonOnPress(
+              AR_NAVIGATOR_TYPE,
+              COLORS.A
+            )}
+            underlayColor={"#68a0ff"}
+          >
+            <Text style={localStyles.buttonText}>{COLORS.A}</Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={localStyles.buttons}
+            onPress={this._getExperienceButtonOnPress(
+              AR_NAVIGATOR_TYPE,
+              COLORS.B
+            )}
+            underlayColor={"#68a0ff"}
+          >
+            <Text style={localStyles.buttonText}>{COLORS.B}</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
+
+  // Returns the ViroARSceneNavigator which will start the AR experience
+  _getARNavigator() {
     return (
       <ViroARSceneNavigator
-        initialScene={{
-          scene: arScenes['player1'],
-        }}
-        apiKey={apiKey} />
-      );
+        {...this.state.sharedProps}
+        initialScene={{ scene: InitialARScene }}
+        viroAppProps={{ color: this.state.color }}
+      />
+    );
+  }
+
+  _getExperienceButtonOnPress(navigatorType, color) {
+    return () => {
+      this.setState({
+        navigatorType: navigatorType,
+        color
+      });
+    };
+  }
+
+  // This function "exits" Viro by setting the navigatorType to UNSET.
+  _exitViro() {
+    this.setState({
+      navigatorType: UNSET
+    });
+  }
+}
+
+var localStyles = StyleSheet.create({
+  viroContainer: {
+    flex: 1,
+    backgroundColor: "black"
+  },
+  outer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "black"
+  },
+  inner: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "black"
+  },
+  titleText: {
+    paddingTop: 30,
+    paddingBottom: 20,
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 25
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 20
+  },
+  buttons: {
+    height: 80,
+    width: 150,
+    paddingTop: 20,
+    paddingBottom: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: "#68a0cf",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#fff"
+  },
+  exitButton: {
+    height: 50,
+    width: 100,
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: "#68a0cf",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#fff"
   }
 });
 
